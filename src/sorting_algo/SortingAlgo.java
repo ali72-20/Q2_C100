504212a97aea627c5390211bc06a07527b2ba2a7
alisafwat <alisafwat11@gmail.com>
2025-03-03 23:48:02 +0200

    SortingAlgo

--- src/sorting_algo/SortingAlgo.java
+++ src/sorting_algo/SortingAlgo.java
@@ -65,5 +65,17 @@ public class SortingAlgo {
             a[mnIndex] = temp;
         }
     }
-    // insertion sort
+    // O(n ^ 2)
+    static public void insertionSort(int[] a){
+        int n = a.length;
+        for(int i = 1; i < n; ++i){
+            int key = a[i];
+            int j = i - 1;
+            while (j >= 0 && a[j] > key){
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = key;
+        }
+    }
 }
