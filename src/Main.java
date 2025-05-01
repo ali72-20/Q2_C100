e18615564a87f5e53126f7c235c8c08070ce1239
alisafwat <alisafwat11@gmail.com>
2025-05-01 19:59:26 +0300

    done: custom set

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-import custom_set.CustomSet;
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        CustomSet set = new CustomSet();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Map<String, Boolean> vis = new HashMap<>();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; ++i) {
+            arr[i] = scanner.next();
+        }
+        for(int i = n - 1; i >=0; --i){
+
+        }
     }
 }
