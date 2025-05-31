7e9c0d23cf5a2e9c62f325e3a5582da5f04987d8
ali72-20 <as20201118@gmail.com>
2025-05-31 19:46:05 +0300

    add: implement number theory utilities and main application

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,7 @@
-import solid.depndancy_inversions.*;
-
 public class Main {
-    private static void showPaymentMenu() {
-        System.out.println("1- cash\n2- Credit card\n3- Vodafone cash\n4- Orange cash\n5- Fawery");
-    }
-
     public static void main(String[] args) {
-        Repo repo = new Repo(new LocalDataBase2(), new RemoteDataBase2());
 
     }
 }
-
-// AiModel // chat with ai
-
-// Chat gpt
-// gemini
-// github copilot
 
 
